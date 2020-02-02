8252f57acda2365bb7981db4e82c24cfd8d443e7
YourName <you@example.com>
2020-02-02 21:07:14 +0900

    group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,8 @@
 class GroupsController < ApplicationController
-
+  before_action :set_group, only: [:edit, :update]
   def index
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -18,11 +18,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to root_path, notice: 'グループを更新しました'
     else
@@ -35,4 +33,7 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:name, user_ids: [])
   end
 
+  def set_group
+    @group = Group.find(params[:id])
+  end
 end
